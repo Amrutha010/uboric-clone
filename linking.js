b93ec60dd739ff3bd61ba92e57e1824ab4a3ab5d
Mohd Anas <mohammadanasquraishi78600@gmail.com>
2022-06-19 20:25:31 +0530

    final commit

--- linking.js
+++ linking.js
@@ -1,5 +1,18 @@
+console.log("linked")
 console.log(document.getElementById("hom"))
 
 document.getElementById("hom").addEventListener("click",function(){
-    location.href="Home.html";
+    location.href="index.html";
 })
+document.querySelector("#Account").addEventListener("click", Account);
+  function Account() {
+    location.href = "./Signup.html";
+  }
+
+  document.getElementById("carts").addEventListener("click",function(){
+    location.href="./cart.html"
+  })
+  console.log("linked 33")
+  document.getElementById("bulk").addEventListener("click",function(){
+    location.href="./login3.html"
+  })
